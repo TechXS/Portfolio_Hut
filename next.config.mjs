93344701577ef221d0d606d59c5df883a9b33725
MesutEmpire <samwainaina01@gmail.com>
2024-05-16 02:07:58 +0300

    Modified yml

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,17 @@
+
+const isGitHubPages = process.env.DEPLOY_TARGET === 'github-pages';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     eslint: {
         // Warning: This allows production builds to successfully complete even if
         // your project has ESLint errors.
+        assetPrefix: isGitHubPages ? 'https://<Account-name>/<Repository-name>' : '',
+        output: "export",
+        reactStrictMode: true,
         ignoreDuringBuilds: true,
     },
 };
 
 export default nextConfig;
+
