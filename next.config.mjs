2aa269aa79e681deae71952034a9bb244389d6f9
MesutEmpire <samwainaina01@gmail.com>
2024-06-08 20:37:57 +0300

    Added the socket server

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,6 @@ const isGitHubPages = process.env.DEPLOY_TARGET === 'github-pages';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "export",
     async headers() {
         return [
             {
